faa11fe5f60d6947d3ae334c2c579be9f7bf0e0f
Juliete Andrade <julie_sp1990@hotmail.com>
2021-06-18 15:37:48 -0300

    mudanças no código cipher/js e index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function encode(e) {
+e.preventDefault();
+  let offset = Number(document.getElementById("keynumber").value);
+  
+
+  let string = document.getElementById("mensagem").value;
+  
+  document.getElementById("resultado").value = cipher.encode(offset, string);
+  
+  
+}
+
+function decode(e) {
+e.preventDefault();
+  let offset = Number(document.getElementById("keynumber").value);
+  let string = document.getElementById("mensagem").value;
+  document.getElementById("resultado").value = cipher.decode(offset, string);
+
+}
+
+let btnEncode = document.getElementById("encodebutton");
+btnEncode.addEventListener("click", encode);
+
+let btnDecode = document.getElementById("decodebutton");
+btnDecode.addEventListener("click", decode);
+
+
